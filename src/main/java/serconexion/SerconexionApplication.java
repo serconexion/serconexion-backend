c48b34ac8bebdfc33a4813745b60f4b6689b40cb
aleja <pacadenae@unal.edu.co>
2020-05-09 18:15:13 -0500

    esqueleto spring

---
+++ src/main/java/serconexion/SerconexionApplication.java
@@ -0,0 +1,13 @@
+package serconexion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerconexionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerconexionApplication.class, args);
+	}
+
+}
